8a545dbd420db0b22e8b81678822fee2a7a8805c
Yashvi Khandelwal <khandelwalyashvi427@gmail.com>
2025-05-18 11:33:54 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const title = "Quizzie Minia";
+const titleContainer = document.getElementById("title");
+
+title.split('').forEach((letter, i) => {
+  const span = document.createElement('span');
+const colors = ["#ff595e", "#ffca3a", "#8ac926", "#1982c4", "#6a4c93", "#f3722c", "#43aa8b", "#b5179e", "#3a86ff", "#ff006e"];
+span.textContent = letter;
+span.style.color = colors[i % colors.length];
+  span.style.animationDelay = `${i * 0.1}s`;
+  titleContainer.appendChild(span);
+});
+
+function startQuiz(topic) {
+  alert(`Start quiz for: ${topic}`);
+}
